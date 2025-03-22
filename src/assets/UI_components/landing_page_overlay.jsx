8de74453936a78c030bb12c8039c01250c02bf04
Kingston Lee <kingstonlee96@gmail.com>
2025-03-22 21:42:57 +0800

    [Fix] Fixed issue with font sizes and layout in mobile view in landing page

--- src/assets/UI_components/landing_page_overlay.jsx
+++ src/assets/UI_components/landing_page_overlay.jsx
@@ -1,23 +1,29 @@
 
 import textBundle from "../../msgbundle";
 
+const SMALL_HEIGHT_ASPECT_RATIO = 2; //width/height 
+
 const LandingPageOverlay = () => {
 
-    return(
-        <div class="absolute z-20  w-full bottom-0 left-0 flex items-end justify-center bg-transparent overflow-hidden">
-           <div class="lg:w-1/2 lg:px-0  md:px-40 px-10 w-full md:h-72 h-50">
+    const screenWithSmallHeight = window.innerWidth/ window.innerHeight > SMALL_HEIGHT_ASPECT_RATIO;
+    const isMobile = window.matchMedia("(any-pointer: coarse)").matches &&  window.innerHeight < 500;
 
-                <p class="md:text-4xl text-2xl text-white text-center">{textBundle["landing.page.welcome"]}</p>
+    
+    return(
+        <div class="absolute z-20  w-full bottom-0 left-0 flex flex-col items-center justify-center bg-transparent overflow-hidden">
+           <div class={`lg:w-1/2 lg:px-0  md:px-40 px-10 ${isMobile ?  "h-50" : "h-72"}  `}>
+
+                <p class={`${isMobile ? "text-2xl" : "text-4xl"}  text-white text-center`}>{textBundle["landing.page.welcome"]}</p>
                 
-                <div class="flex justify-center md:mt-2 mt-1">
+                <div class={`flex justify-center ${isMobile ? screenWithSmallHeight ? "mt-0": "mt-1" : "mt-2"}`}>
                     <div class='h-5 bg-transparent border-t-3 border-t-white w-25 opacity-50'></div>
 
                 </div>
 
-                <p className="text-white text-center text-sm md:text-base">
+                <p className={`text-white text-center ${isMobile ? "text-sm" : "text-base"}`}>
                     {textBundle["landing.page.welcome.message"]}
                 </p>
-                <div class='md:h-8 h-2 bg-transparent '></div>
+                <div class={`  ${isMobile ? "h-4": "h-8"} bg-transparent `}></div>
 
                 <div className="flex justify-center items-center">
                     <a className='enter-simulation-btn' href='/simulation'>

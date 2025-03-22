2a26b592c2fc3b278837027edb71d64edaacde13
Kingston Lee <kingstonlee96@gmail.com>
2025-03-22 21:58:40 +0800

    [Changes] Updated notification panel to cater for mobile

--- src/assets/UI_components/notification.jsx
+++ src/assets/UI_components/notification.jsx
@@ -1,14 +1,21 @@
 import { motion } from 'framer-motion';
 import closeBtn from '../icons/notification_close.png';
 
+const SMALL_HEIGHT_ASPECT_RATIO = 2; //width/height 
+
+
 const NotificationPanel = ({ closeNotificationCallback, content, icon }) => {
+    const screenWithSmallHeight = window.innerWidth/ window.innerHeight > SMALL_HEIGHT_ASPECT_RATIO;
+    const isMobile = window.matchMedia("(any-pointer: coarse)").matches &&  window.innerHeight < 500;
+
+
     return (
         <motion.div 
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -50 }}
             transition={{ duration: 0.8 }}
-            className="notification-panel"
+            className={isMobile ? "notification-panel-mobile" :"notification-panel"} 
         >
             <div className='flex justify-end items-center cursor-pointer' onClick={closeNotificationCallback}>
                 <img className='h-4 w-4' src={closeBtn} alt="close" />
@@ -18,7 +25,7 @@ const NotificationPanel = ({ closeNotificationCallback, content, icon }) => {
                     <img className='composure-effect-img' src={icon} alt="icon" />
                 </div>
                 <div className='w-6'></div>
-                <div>
+                <div className={`${isMobile ? "text-xs" : "text-base"}`}>
                     {content}
                 </div>
             </div>
